3da216860741044324ceb9c67f94afaa52c01a16
tolip05 <kosta1980@abv.bg>
2018-10-18 11:56:18 +0300

    commit

---
+++ source/repos/DefiningClasses/FamilyTree/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyTree
+{
+   public class Person
+    {
+        private string name;
+        private string birthDay;
+        private List<Person> parents;
+        private List<Person> children;
+        public Person(string name,string birthDay)
+        {
+            this.Name = name;
+            this.BirthDay = birthDay;
+            this.Parents = new List<Person>();
+            this.Children = new List<Person>();
+        }
+
+
+        
+
+        public List<Person> Children
+        {
+            get { return children; }
+            set { children = value; }
+        }
+
+        public List<Person> Parents
+        {
+            get { return parents; }
+            set { parents = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+
+        public string BirthDay
+        {
+            get { return birthDay; }
+            set { birthDay = value; }
+        }
+
+    }
+}
---
+++ source/repos/DefiningClasses/FamilyTree/StartUp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FamilyTree
+{
+   public class StartUp
+    {
+        static List<Person> persons;
+        static List<string> releitionShips;
+      public  static void Main(string[] args)
+        {
+            persons = new List<Person>();
+            releitionShips = new List<string>();
+            string namePerson = Console.ReadLine();
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (!input.Contains("-"))
+                {
+                    AddMember(input);
+                    input = Console.ReadLine();
+                    continue;
+                }
+                releitionShips.Add(input);
+
+                input = Console.ReadLine();
+            }
+            foreach (var item in releitionShips)
+            {
+                string[] inputArgs = item.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+                Person parent = GetPerson(inputArgs[0]);
+                Person chaeld = GetPerson(inputArgs[1]);
+                if (!parent.Children.Contains(chaeld))
+                {
+                    parent.Children.Add(chaeld);
+                }
+                if (!chaeld.Parents.Contains(parent))
+                {
+                    chaeld.Parents.Add(parent);
+      
+                }
+            }
+            Print(namePerson);
+        }
+
+        private static void Print(string namePerson)
+        {
+            Person mainPerson = GetPerson(namePerson);
+
+            Console.WriteLine($"{mainPerson.Name} {mainPerson.BirthDay}");
+            Console.WriteLine("Parents:");
+            foreach (var parent in mainPerson.Parents)
+            {
+                Console.WriteLine($"{parent.Name} {parent.BirthDay}");
+            }
+            Console.WriteLine("Children:");
+            foreach (var chield in mainPerson.Children)
+            {
+                Console.WriteLine($"{chield.Name} {chield.BirthDay}");
+            }
+        }
+
+        private static Person GetPerson(string input)
+        {
+            if (input.Contains('/'))
+            {
+                return persons.FirstOrDefault(x => x.BirthDay == input);
+            }
+            return persons.FirstOrDefault(x => x.Name == input);
+        }
+
+        private static void AddMember(string input)
+        {
+            string[] inputArgs = input.Split();
+            string name = inputArgs[0] +" "+ inputArgs[1];
+            string birthDay = inputArgs[2];
+
+            Person person = new Person(name,birthDay);
+            persons.Add(person);
+        }
+    }
+}
